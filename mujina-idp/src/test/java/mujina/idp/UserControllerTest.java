39aabbe78a2314a95c5e1f7c03d95dff7f1481c5
Barry Evans <barry.evans@version1.com>
2018-03-20 08:29:08 +0000

    Removing access for default users (admin/user) - added application.yml for integration tests - changing intergartion tests to use testuser, not admin - add testuser attribute via api call

--- mujina-idp/src/test/java/mujina/idp/UserControllerTest.java
+++ mujina-idp/src/test/java/mujina/idp/UserControllerTest.java
@@ -3,7 +3,6 @@ package mujina.idp;
 import io.restassured.filter.cookie.CookieFilter;
 import mujina.AbstractIntegrationTest;
 import org.junit.Test;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 import static io.restassured.RestAssured.given;
@@ -15,32 +14,91 @@ import static org.hamcrest.core.StringContains.containsString;
 public class UserControllerTest extends AbstractIntegrationTest {
 
   @Test
-  public void user() throws Exception {
-    doUser("/user.html");
+  public void userResourceForAdmin() throws Exception {
+    CookieFilter cookieFilter = login("admin", "secret", SC_MOVED_TEMPORARILY);
+
+    given()
+      .filter(cookieFilter)
+      .get("/user.html")
+      .then()
+      .statusCode(SC_OK)
+      .body(
+        containsString("<h1>LAA Portal Mock Identity Provider</h1>"),
+        containsString("<form id=\"loginData\" method=\"post\" action=\"/login\">"));
   }
 
   @Test
-  public void index() throws Exception {
-    doUser("/");
+  public void userResourceForUser() throws Exception {
+    CookieFilter cookieFilter = login("user", "secret", SC_MOVED_TEMPORARILY);
+
+    given()
+      .filter(cookieFilter)
+      .get("/user.html")
+      .then()
+      .statusCode(SC_OK)
+      .body(
+        containsString("<h1>LAA Portal Mock Identity Provider</h1>"),
+        containsString("<form id=\"loginData\" method=\"post\" action=\"/login\">"));
   }
 
   @Test
-  public void indexNotLoggedIn() throws Exception {
+  public void userResourceForTestUser() throws Exception {
+    CookieFilter cookieFilter = login("testuser", "secret", SC_MOVED_TEMPORARILY);
+
+    given()
+      .filter(cookieFilter)
+      .get("/user.html")
+      .then()
+      .statusCode(SC_OK)
+      .body(containsString("testuser"));
+  }
+
+  @Test
+  public void indexResourceForAdmin() throws Exception {
+    CookieFilter cookieFilter = login("admin", "secret", SC_MOVED_TEMPORARILY);
+
+    given()
+      .filter(cookieFilter)
+      .get("/")
+      .then()
+      .statusCode(SC_OK)
+      .body(
+        containsString("<h1>LAA Portal Mock Identity Provider</h1>"),
+        containsString("<a href=\"/user.html\">Login</a>"));
+  }
+
+  @Test
+  public void indexResourceForUser() throws Exception {
+    CookieFilter cookieFilter = login("user", "secret", SC_MOVED_TEMPORARILY);
+
+    given()
+      .filter(cookieFilter)
+      .get("/")
+      .then()
+      .statusCode(SC_OK)
+      .body(
+        containsString("<h1>LAA Portal Mock Identity Provider</h1>"),
+        containsString("<a href=\"/user.html\">Login</a>"));
+  }
+
+  @Test
+  public void indexResourceForTestUser() throws Exception {
+    CookieFilter cookieFilter = login("testuser", "secret", SC_MOVED_TEMPORARILY);
+
+    given()
+      .filter(cookieFilter)
+      .get("/")
+      .then()
+      .statusCode(SC_OK)
+      .body(containsString("testuser"));
+  }
+
+  @Test
+  public void indexResourceForNotLoggedIn() {
     given()
       .get("/")
       .then()
       .statusCode(SC_OK)
       .body(containsString("Login"));
-  }
-
-  private void doUser(String path) throws Exception {
-    CookieFilter cookieFilter = login("admin", "secret", SC_MOVED_TEMPORARILY);
-
-    given()
-      .filter(cookieFilter)
-      .get(path)
-      .then()
-      .statusCode(SC_OK)
-      .body(containsString("admin"));
   }
 }
